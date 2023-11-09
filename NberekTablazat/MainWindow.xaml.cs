ff4f74024efa8351e1105c0160437e81ea30427a
Petrik Diak <diak@petrik.hu>
2023-11-09 08:26:46 +0100

    Add project files.

---
+++ NberekTablazat/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NberekTablazat
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		List<Nber> lista = new List<Nber>();
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			lista.Add(new Nber("Vella Nándor", 42));
+			lista.Add(new Nber("Lakatos Béla", 17));
+			lista.Add(new Nber("Kovács Ferenc", 56));
+
+			nberek.ItemsSource = lista;
+		}
+
+		private void Button_HozzáAd_Click(object sender, RoutedEventArgs e)
+		{
+			string name = TBName.Text;
+			int age = Convert.ToInt32(TBAge.Text);
+
+			MessageBox.Show(name + "+" + age);
+			Nber nber = new Nber(name, age);
+			lista.Add(nber);
+			nberek.Items.Refresh();
+
+
+
+		}
+	}
+}
---
+++ NberekTablazat/Nber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NberekTablazat
+{
+	internal class Nber
+	{
+		private string nev;
+		private int kor;
+
+		public Nber(string nev, int kor)
+		{
+			this.nev = nev;
+			this.kor = kor;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public int Kor { get => kor; set => kor = value; }
+	}
+}
